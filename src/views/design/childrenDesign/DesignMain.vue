2b2a364cae77e41e5455e5302652fdbe2e92e6fa
yellowGw0 <2356265853@qq.com>
2020-11-28 09:43:14 +0800

    9:43

--- src/views/design/childrenDesign/DesignMain.vue
+++ src/views/design/childrenDesign/DesignMain.vue
@@ -5,13 +5,82 @@
       <li>全部</li>
     </ul>
     <div class="design-content">
+      <div v-for="(item, index) in designList" :key="index">
+        {{item.name}}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DesignMain'
+  name: 'DesignMain',
+  data () {
+    return {
+      designList: [
+        {
+          name: '1',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '2',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '3',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '4',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        },
+        {
+          name: '5',
+          imgUrl: '',
+          link: '123',
+          text: '我是'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -23,9 +92,12 @@ export default {
   }
   .design-content {
     background-color: var(--color-background);
-    height: 985px;
+    height: 1285px;
     border-radius: 20px;
     overflow: hidden;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
   }
   .design-content-title {
     font-size: 26px;
@@ -33,14 +105,27 @@ export default {
     display: flex;
     justify-content: center;
     font-weight: 400;
-    border-bottom: 1px solid #000;
+    border-bottom: 1px solid rgb(121, 121, 121);
   }
   .design-content-title li {
     padding: 20px;
     letter-spacing: 8px;
     cursor: pointer;
   }
-    
-  
-
+  .design-content div{
+    border-radius: 20px;
+    width: 350px;
+    height: 400px;
+    margin: 10px;
+    background-color: red;
+    cursor: pointer;
+    color: #fff;
+    font-size: 24px;
+    line-height: 400px;
+    text-align: center;
+    transition: .2s;
+  }
+  .design-content div:hover {
+    background-color: rgb(138, 0, 0);
+  }
 </style>
