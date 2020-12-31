a3bb2d9470161819af27722580ddfcaa7298915a
yellowGw0 <2356265853@qq.com>
2020-12-31 14:11:44 +0800

    login-change

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="login">
     <div class="login-content">
-      <span></span>
+      <i class="login-icon"></i>
       <form action="">
         <div class="login-name">
           <i class="login-name-icon login-icon-style"></i>
-          <input type="text" class="login-input">
+          <input type="text" class="login-input" placeholder="请输入用户名">
         </div>
         <div class="login-pass">
           <i class="login-pass-icon login-icon-style"></i>
-          <input type="text" class="login-input">
+          <input type="text" class="login-input" placeholder="请输入密码">
         </div>
-        <p>忘记密码？</p>
-        <p><input type="submit" class="login-submit" value="登录"></p>
+        <p class="forget"><a href="javascript:;">忘记密码？</a></p>
+        <p class="login-button"><input type="submit" class="login-submit" value="登录"></p>
       </form>
       <p>
-      <span>注册</span>
+      <span class="login-register">注册</span>
       </p>
     </div>
   </div>
@@ -50,7 +50,19 @@ export default {
     top: 50%;
     transform: translate(0px, -50%);
     border-radius: 20px;
-    /* position: relative; */
+  }
+  .login-icon {
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background-image: url('../../assets/img/login-icon.png');
+    background-size: contain;
+    position: absolute;
+    /* top: 50%; */
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+    top: 10%;
   }
   .login-icon-style {
     display: inline-block;
@@ -63,7 +75,7 @@ export default {
   .login-name {
     position: absolute;
     left: 50%;
-    /* top: 50%; */
+    top: 30%;
     transform: translateX(-50%);
     display: flex;
   }
@@ -71,7 +83,7 @@ export default {
     display: flex;
     position: absolute;
     left: 50%;
-    top: 100px;
+    top: 50%;
     transform: translateX(-50%);
   }
   .login-name-icon {
@@ -86,5 +98,49 @@ export default {
     height: 40px;
     border-bottom: 1px solid #777777;
     border-radius: 0px;
+  }
+  .forget {
+    position: absolute;
+    top: 57%;
+    right: 12%;
+    color: var(--color-background-blue);
+  }
+  .login-button {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 27%;
+  }
+  .login-button input {
+    width: 150px;
+    height: 50px;
+    background-color: var(--color-background-blue-unselected);
+    color: #fff;
+    border-radius: 10px;
+    font-size: 22px;
+    letter-spacing: 2px;
+    cursor: pointer;
+    transition: .2s;
+    box-shadow: 0px 0px 8px #3d3d3d59;
+  }
+  .login-button input:hover {
+    width: 350px;
+    background-color: var(--color-background-blue);
+  }
+  .login-register {
+    position: absolute;
+    bottom: 15%;
+    left: 50%;
+    transform: translateX(-50%);
+    border-bottom: 1px solid var(--color-background-blue);
+    color: var(--color-background-blue);
+    font-size: 16px;
+    line-height: 15px;
+    cursor: pointer;
+    transition: .1s;
+  }
+  .login-register:hover {
+    color: red;
+    border-bottom-color: red;
   }
 </style>
